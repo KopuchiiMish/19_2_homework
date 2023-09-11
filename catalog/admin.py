de3604f024759bc5d12d49d6e0cc5aee5e636032
nataliagovorushkina <a10061514@gmail.com>
2023-09-11 23:03:25 +0300

    homework 20_1_2

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    text_fields = ('name', 'description', 'price', 'image',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    text_fields = ('name',)
