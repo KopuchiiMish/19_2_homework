4a4315150a976c691068dbfe72260cb793024d60
nataliagovorushkina <a10061514@gmail.com>
2023-09-11 23:40:18 +0300

    homework 20_1_5

--- catalog/admin.py
+++ catalog/admin.py
@@ -5,9 +5,11 @@ from catalog.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    text_fields = ('name', 'description', 'price', 'image',)
+    list_display = ('id', 'name', 'price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    text_fields = ('name',)
+    list_display = ('id', 'name',)
