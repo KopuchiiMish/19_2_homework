5e16c422132676fee9b4b63d1da79efcdfc9a544
nataliagovorushkina <a10061514@gmail.com>
2023-10-19 23:15:57 +0300

    homework_23_1

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-
 from catalog.models import *
 from users.models import User
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category',)
+    list_display = ('name', 'description', 'category', 'is_published')
     list_filter = ('category',)
-    search_fields = ('name', 'description',)
+    list_editable = ('description', 'category', 'is_published')
 
 
 @admin.register(Category)
